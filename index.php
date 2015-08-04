b244961b56859c38895d821e2fa758849e6fc58d
Paweł Jedwabny <pjedwabny@gmail.com>
2015-08-04 20:56:16 +0200

    add bootstrap

--- index.php
+++ index.php
@@ -99,7 +99,8 @@ Please choose <?php print(EPESI);?> version:<ul>
 		require_once('Minify/Build.php');
 		$jquery = DEBUG_JS ? 'libs/jquery-1.11.3.js' : 'libs/jquery-1.11.3.min.js';
 		$jquery_migrate = DEBUG_JS ? 'libs/jquery-migrate-1.2.1.js' : 'libs/jquery-migrate-1.2.1.min.js';
-		$jses = array('libs/prototype.js', $jquery, $jquery_migrate, 'libs/jquery-ui-1.10.1.custom.min.js', 'libs/HistoryKeeper.js', 'include/epesi.js');
+		$bootstrap = DEBUG_JS ? 'libs/bootstrap/js/bootstrap.js' : 'libs/bootstrap/js/bootstrap.min.js';
+		$jses = array('libs/prototype.js', $jquery, $jquery_migrate, $bootstrap, 'libs/jquery-ui-1.10.1.custom.min.js', 'libs/HistoryKeeper.js', 'include/epesi.js');
 	if(!DEBUG_JS) {
 		$jsses_build = new Minify_Build($jses);
 		$jsses_src = $jsses_build->uri('serve.php?' . http_build_query(array('f' => array_values($jses))));
@@ -113,6 +114,7 @@ Please choose <?php print(EPESI);?> version:<ul>
 	$csses_src = $csses_build->uri('serve.php?'.http_build_query(array('f'=>array_values($csses))));
 ?>
 		<link type="text/css" href="<?php print($csses_src)?>" rel="stylesheet"></link>
+		<link type="text/css" href="libs/bootstrap/css/bootstrap.css" rel="stylesheet"/>
 
 		<style type="text/css">
 			<?php if (DIRECTION_RTL) print('body { direction: rtl; }'); ?>
