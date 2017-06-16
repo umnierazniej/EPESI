417c9b96e645fa66f2cfb65bf7414068fde18228
ZiebaM <zieba.mic@gmail.com>
2017-06-16 21:12:33 +0200

    [fix] added condition to check if class exists

--- modules/Utils/RecordBrowser/RecordBrowser_0.php
+++ modules/Utils/RecordBrowser/RecordBrowser_0.php
@@ -1376,10 +1376,12 @@ class Utils_RecordBrowser extends Module {
                         $lk_c = -strlen($lk);
                         foreach ($ml as $k=>$v){
                             if(strtolower(substr($k,$lk_c)) == $lk) {
-                                $menu = call_user_func([$k.'Common','menu']);
-                                foreach ($menu as $key => $value ){
-                                    foreach($value as $kk => $vv) {
-                                        if($kk != '__submenu__' && $kk != '__icon__' && isset($vv['__icon__'])) $this->icon = $vv['__icon__'];
+                                if(class_exists($k.'Common')) {
+                                    $menu = call_user_func([$k . 'Common', 'menu']);
+                                    foreach ($menu as $key => $value) {
+                                        foreach ($value as $kk => $vv) {
+                                            if ($kk != '__submenu__' && $kk != '__icon__' && isset($vv['__icon__'])) $this->icon = $vv['__icon__'];
+                                        }
                                     }
                                 }
                             }
